bf19c9a2271a41a7353afef871351516d6ac991c
Md. Jahin Khan Utsho <159694422+jahinutsho@users.noreply.github.com>
2025-08-14 12:06:37 +0600

    update 11

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { Admin } from '../admin/entities/admin.entity';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { User } from './entities/user.entity';
 import { EmailService } from './email/email.service';
+import { Admin } from '../admin/entities/admin.entity';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Admin,User]),
-    JwtModule.register({
-      secret: 'SECRET_KEY',
-      signOptions: { expiresIn: '1d' },
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([Admin, User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
